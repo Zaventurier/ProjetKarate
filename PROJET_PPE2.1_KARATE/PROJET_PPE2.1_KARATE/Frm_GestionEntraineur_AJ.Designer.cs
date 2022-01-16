be4613cf75caec6d8ad0258f87d000a470e478b5
Thierry Bogusz <vous@exemple.com>
2022-01-16 13:14:04 +0000

    ap2.1_equipe1.git

---
+++ PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_GestionEntraineur_AJ.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace PROJET_PPE2._1_KARATE
+{
+    partial class Frm_GestionEntraineur_AJ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBox3 = new System.Windows.Forms.GroupBox();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.TextBox4 = new System.Windows.Forms.TextBox();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.TextBox3 = new System.Windows.Forms.TextBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.GroupBox3.SuspendLayout();
+            this.GroupBox2.SuspendLayout();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBox3
+            // 
+            this.GroupBox3.Controls.Add(this.Button2);
+            this.GroupBox3.Controls.Add(this.Button1);
+            this.GroupBox3.Location = new System.Drawing.Point(111, 324);
+            this.GroupBox3.Name = "GroupBox3";
+            this.GroupBox3.Size = new System.Drawing.Size(578, 82);
+            this.GroupBox3.TabIndex = 15;
+            this.GroupBox3.TabStop = false;
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(309, 25);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(195, 42);
+            this.Button2.TabIndex = 1;
+            this.Button2.Text = "FERMER";
+            this.Button2.UseVisualStyleBackColor = true;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(80, 25);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(200, 42);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "AJOUTER";
+            this.Button1.UseVisualStyleBackColor = true;
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.TextBox2);
+            this.GroupBox2.Controls.Add(this.Label2);
+            this.GroupBox2.Controls.Add(this.TextBox4);
+            this.GroupBox2.Controls.Add(this.TextBox1);
+            this.GroupBox2.Controls.Add(this.Label5);
+            this.GroupBox2.Controls.Add(this.Label3);
+            this.GroupBox2.Controls.Add(this.TextBox3);
+            this.GroupBox2.Controls.Add(this.Label4);
+            this.GroupBox2.Location = new System.Drawing.Point(111, 124);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(578, 194);
+            this.GroupBox2.TabIndex = 14;
+            this.GroupBox2.TabStop = false;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(176, 65);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(241, 26);
+            this.TextBox2.TabIndex = 4;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(16, 22);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(49, 20);
+            this.Label2.TabIndex = 1;
+            this.Label2.Text = "NOM:";
+            // 
+            // TextBox4
+            // 
+            this.TextBox4.Location = new System.Drawing.Point(176, 155);
+            this.TextBox4.Name = "TextBox4";
+            this.TextBox4.Size = new System.Drawing.Size(241, 26);
+            this.TextBox4.TabIndex = 8;
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(176, 22);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(241, 26);
+            this.TextBox1.TabIndex = 2;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(16, 155);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(155, 20);
+            this.Label5.TabIndex = 7;
+            this.Label5.Text = "VILLE NAISSANCE:";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(16, 65);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(82, 20);
+            this.Label3.TabIndex = 3;
+            this.Label3.Text = "PRENOM:";
+            // 
+            // TextBox3
+            // 
+            this.TextBox3.Location = new System.Drawing.Point(176, 111);
+            this.TextBox3.Name = "TextBox3";
+            this.TextBox3.Size = new System.Drawing.Size(241, 26);
+            this.TextBox3.TabIndex = 6;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(16, 111);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(153, 20);
+            this.Label4.TabIndex = 5;
+            this.Label4.Text = "DATE NAISSANCE:";
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.Label1);
+            this.GroupBox1.Location = new System.Drawing.Point(111, 45);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(578, 72);
+            this.GroupBox1.TabIndex = 13;
+            this.GroupBox1.TabStop = false;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(15, 22);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(538, 32);
+            this.Label1.TabIndex = 10;
+            this.Label1.Text = "GESTION DES MEMBRES [AJOUTER]";
+            // 
+            // Frm_GestionEntraineur_AJ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GroupBox3);
+            this.Controls.Add(this.GroupBox2);
+            this.Controls.Add(this.GroupBox1);
+            this.Name = "Frm_GestionEntraineur_AJ";
+            this.Text = "Formulaire_charte_graphique";
+            this.GroupBox3.ResumeLayout(false);
+            this.GroupBox2.ResumeLayout(false);
+            this.GroupBox2.PerformLayout();
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.GroupBox GroupBox3;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox TextBox4;
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox TextBox3;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.Label Label1;
+    }
+}
---
+++ PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_GestionEntraineur_AJ.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJET_PPE2._1_KARATE
+{
+    public partial class Frm_GestionEntraineur_AJ : Form
+    {
+        public Frm_GestionEntraineur_AJ()
+        {
+            InitializeComponent();
+        }
+    }
+}
