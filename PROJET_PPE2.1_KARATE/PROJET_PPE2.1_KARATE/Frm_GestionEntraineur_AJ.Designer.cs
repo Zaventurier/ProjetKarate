42d35b4ee080fa51f934739c853d51b59c4c5bbb
PASCAIL Guillaume <pascailguillaumepro@gmail.com>
2022-01-27 09:27:25 +0100

    xxx

--- PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_GestionEntraineur_AJ.Designer.cs
+++ PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_GestionEntraineur_AJ.Designer.cs
@@ -51,30 +51,30 @@
             // 
             this.GroupBox3.Controls.Add(this.Button2);
             this.GroupBox3.Controls.Add(this.Button1);
-            this.GroupBox3.Location = new System.Drawing.Point(74, 211);
-            this.GroupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.GroupBox3.Location = new System.Drawing.Point(99, 260);
+            this.GroupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.GroupBox3.Name = "GroupBox3";
-            this.GroupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.GroupBox3.Size = new System.Drawing.Size(385, 53);
+            this.GroupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.GroupBox3.Size = new System.Drawing.Size(513, 65);
             this.GroupBox3.TabIndex = 15;
             this.GroupBox3.TabStop = false;
             // 
             // Button2
             // 
-            this.Button2.Location = new System.Drawing.Point(206, 16);
-            this.Button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Button2.Location = new System.Drawing.Point(275, 20);
+            this.Button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Button2.Name = "Button2";
-            this.Button2.Size = new System.Drawing.Size(130, 27);
+            this.Button2.Size = new System.Drawing.Size(173, 33);
             this.Button2.TabIndex = 1;
             this.Button2.Text = "FERMER";
             this.Button2.UseVisualStyleBackColor = true;
             // 
             // Button1
             // 
-            this.Button1.Location = new System.Drawing.Point(53, 16);
-            this.Button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Button1.Location = new System.Drawing.Point(71, 20);
+            this.Button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Button1.Name = "Button1";
-            this.Button1.Size = new System.Drawing.Size(133, 27);
+            this.Button1.Size = new System.Drawing.Size(177, 33);
             this.Button1.TabIndex = 0;
             this.Button1.Text = "AJOUTER";
             this.Button1.UseVisualStyleBackColor = true;
@@ -89,94 +89,90 @@
             this.GroupBox2.Controls.Add(this.Label3);
             this.GroupBox2.Controls.Add(this.TextBox3);
             this.GroupBox2.Controls.Add(this.Label4);
-            this.GroupBox2.Location = new System.Drawing.Point(74, 81);
-            this.GroupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.GroupBox2.Location = new System.Drawing.Point(99, 100);
+            this.GroupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.GroupBox2.Name = "GroupBox2";
-            this.GroupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.GroupBox2.Size = new System.Drawing.Size(385, 126);
+            this.GroupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.GroupBox2.Size = new System.Drawing.Size(513, 155);
             this.GroupBox2.TabIndex = 14;
             this.GroupBox2.TabStop = false;
             // 
             // TextBox2
             // 
-            this.TextBox2.Location = new System.Drawing.Point(117, 42);
-            this.TextBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TextBox2.Location = new System.Drawing.Point(156, 52);
+            this.TextBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TextBox2.Name = "TextBox2";
-            this.TextBox2.Size = new System.Drawing.Size(162, 20);
+            this.TextBox2.Size = new System.Drawing.Size(215, 22);
             this.TextBox2.TabIndex = 4;
             // 
             // Label2
             // 
             this.Label2.AutoSize = true;
-            this.Label2.Location = new System.Drawing.Point(11, 14);
-            this.Label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label2.Location = new System.Drawing.Point(15, 17);
             this.Label2.Name = "Label2";
-            this.Label2.Size = new System.Drawing.Size(35, 13);
+            this.Label2.Size = new System.Drawing.Size(44, 17);
             this.Label2.TabIndex = 1;
             this.Label2.Text = "NOM:";
             // 
             // TextBox4
             // 
-            this.TextBox4.Location = new System.Drawing.Point(117, 101);
-            this.TextBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TextBox4.Location = new System.Drawing.Point(156, 124);
+            this.TextBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TextBox4.Name = "TextBox4";
-            this.TextBox4.Size = new System.Drawing.Size(162, 20);
+            this.TextBox4.Size = new System.Drawing.Size(215, 22);
             this.TextBox4.TabIndex = 8;
             // 
             // TextBox1
             // 
-            this.TextBox1.Location = new System.Drawing.Point(117, 14);
-            this.TextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TextBox1.Location = new System.Drawing.Point(156, 17);
+            this.TextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TextBox1.Name = "TextBox1";
-            this.TextBox1.Size = new System.Drawing.Size(162, 20);
+            this.TextBox1.Size = new System.Drawing.Size(215, 22);
             this.TextBox1.TabIndex = 2;
             // 
             // Label5
             // 
             this.Label5.AutoSize = true;
-            this.Label5.Location = new System.Drawing.Point(11, 101);
-            this.Label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label5.Location = new System.Drawing.Point(15, 124);
             this.Label5.Name = "Label5";
-            this.Label5.Size = new System.Drawing.Size(103, 13);
+            this.Label5.Size = new System.Drawing.Size(130, 17);
             this.Label5.TabIndex = 7;
             this.Label5.Text = "VILLE NAISSANCE:";
             // 
             // Label3
             // 
             this.Label3.AutoSize = true;
-            this.Label3.Location = new System.Drawing.Point(11, 42);
-            this.Label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label3.Location = new System.Drawing.Point(15, 52);
             this.Label3.Name = "Label3";
-            this.Label3.Size = new System.Drawing.Size(57, 13);
+            this.Label3.Size = new System.Drawing.Size(72, 17);
             this.Label3.TabIndex = 3;
             this.Label3.Text = "PRENOM:";
             // 
             // TextBox3
             // 
-            this.TextBox3.Location = new System.Drawing.Point(117, 72);
-            this.TextBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TextBox3.Location = new System.Drawing.Point(156, 89);
+            this.TextBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TextBox3.Name = "TextBox3";
-            this.TextBox3.Size = new System.Drawing.Size(162, 20);
+            this.TextBox3.Size = new System.Drawing.Size(215, 22);
             this.TextBox3.TabIndex = 6;
             // 
             // Label4
             // 
             this.Label4.AutoSize = true;
-            this.Label4.Location = new System.Drawing.Point(11, 72);
-            this.Label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label4.Location = new System.Drawing.Point(15, 89);
             this.Label4.Name = "Label4";
-            this.Label4.Size = new System.Drawing.Size(103, 13);
+            this.Label4.Size = new System.Drawing.Size(130, 17);
             this.Label4.TabIndex = 5;
             this.Label4.Text = "DATE NAISSANCE:";
             // 
             // GroupBox1
             // 
             this.GroupBox1.Controls.Add(this.Label1);
-            this.GroupBox1.Location = new System.Drawing.Point(74, 29);
-            this.GroupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.GroupBox1.Location = new System.Drawing.Point(99, 36);
+            this.GroupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.GroupBox1.Name = "GroupBox1";
-            this.GroupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.GroupBox1.Size = new System.Drawing.Size(385, 47);
+            this.GroupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.GroupBox1.Size = new System.Drawing.Size(513, 58);
             this.GroupBox1.TabIndex = 13;
             this.GroupBox1.TabStop = false;
             // 
@@ -184,25 +180,25 @@
             // 
             this.Label1.AutoSize = true;
             this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label1.Location = new System.Drawing.Point(10, 14);
-            this.Label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label1.Location = new System.Drawing.Point(13, 17);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(369, 24);
+            this.Label1.Size = new System.Drawing.Size(465, 29);
             this.Label1.TabIndex = 10;
             this.Label1.Text = "GESTION DES MEMBRES [AJOUTER]";
             // 
             // Frm_GestionEntraineur_AJ
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.ClientSize = new System.Drawing.Size(711, 359);
             this.Controls.Add(this.GroupBox3);
             this.Controls.Add(this.GroupBox2);
             this.Controls.Add(this.GroupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Frm_GestionEntraineur_AJ";
             this.Text = "Formulaire_charte_graphique";
+            this.Load += new System.EventHandler(this.Frm_GestionEntraineur_AJ_Load_1);
             this.GroupBox3.ResumeLayout(false);
             this.GroupBox2.ResumeLayout(false);
             this.GroupBox2.PerformLayout();
--- PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_GestionEntraineur_AJ.cs
+++ PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Frm_GestionEntraineur_AJ.cs
@@ -101,5 +101,10 @@ namespace PROJET_PPE2._1_KARATE
         {
 
         }
+
+        private void Frm_GestionEntraineur_AJ_Load_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
