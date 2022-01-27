8fa1e9847c62ff61d041bebc66e94cf696ceccb3
Faure Léo <leoaxel.faure.87@gmail.com>
2022-01-27 10:25:00 +0100

    version indev 2.0.1 - correctif erreur de connexion git + ajout form recherche

--- PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Form1.cs
+++ PROJET_PPE2.1_KARATE/PROJET_PPE2.1_KARATE/Form1.cs
@@ -16,5 +16,30 @@ namespace PROJET_PPE2._1_KARATE
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
